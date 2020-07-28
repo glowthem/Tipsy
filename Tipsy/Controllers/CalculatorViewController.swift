da0eaca1675099973f21dcde6bbc26cab58a65e0
glowthem <glowing713@naver.com>
2020-07-28 23:34:27 +0900

    2020-07-28 ➕ button select, tip percentage string to double

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -1,21 +1,34 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-
+    var tip = 0.10
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        let selected = sender.currentTitle!
+        let selectedDecimal = String(selected.dropLast())
+        
+        let selectedDouble = Double(selectedDecimal)!
+        tip = selectedDouble / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tip)
     }
     
 }
